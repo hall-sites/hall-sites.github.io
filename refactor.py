9f3ccf4cf84720f0d063494fc6278b0baf09be9e
= <=>
2023-05-29 21:39:13 -0400

    test basetest.html

---
+++ refactor.py
@@ -0,0 +1,3 @@
+MATH_SITES = ['factoring.html', 'factoring2.html', 'LinearFlashCards.html', 'SolveSystElimFlash.html',  'exponents.html', 'quadform.html',
+              'SolveLinEqDist.html', 'SolveLinWithDistFlashCards', 'ThreeStepLinearEquationFlashCards.html', 'SolveSystSubFlash.html', 'SolveLinEqCLT.html']
+
